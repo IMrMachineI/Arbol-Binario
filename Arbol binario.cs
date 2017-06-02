3e6f4140ea6f00833d80e4b1331f1340c9419c4d
IMrMachineI <fubu96meex@gmail.com>
2017-06-01 19:22:35 -0500

    Add files via upload

---
+++ Arbol binario.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbol_Binario
+{
+    class Arbol_binario
+    {
+        private Nodo raiz;
+        public void agregarNodo(Nodo nuevoNodo)
+        {
+            if(raiz == null)
+            {
+                raiz = nuevoNodo;
+            }
+            else
+            {
+                agregar(nuevoNodo, raiz);
+            }
+        }
+        private void agregar(Nodo nodo,Nodo padre)
+        {
+            if(nodo.dato < padre.dato)
+            {
+                if(padre.hijoIzquierdo == null)
+                {
+                    padre.hijoIzquierdo = nodo;
+                }
+                else
+                {
+                    agregar(nodo, padre.hijoIzquierdo);
+                }
+            }
+            else
+            {
+                if(padre.hijoDerecho == null)
+                {
+                    padre.hijoDerecho = nodo;
+                }
+                else
+                {
+                    agregar(nodo, padre.hijoDerecho);
+                }
+            }
+        }
+        public string PreOrden()
+        {
+            if(raiz == null)
+            {
+                return "";
+            }
+            else
+            {
+                return PreOrden(raiz);
+            }
+        }
+        private string PreOrden(Nodo raiz)
+        {
+            string resultado = "";
+            resultado += raiz.ToString();
+            if(raiz.hijoIzquierdo != null)
+            {
+                resultado += PreOrden(raiz.hijoIzquierdo);
+                if(raiz.hijoDerecho != null)
+                {
+                    resultado += PreOrden(raiz.hijoDerecho);
+                }
+            }
+            return resultado;
+        }
+        public string InOrden()
+        {
+            if (raiz == null)
+            {
+                return "";
+            }
+            else
+            {
+                return InOrden(raiz);
+            }
+        }
+        private string InOrden(Nodo raiz)
+        {
+            string resultado = "";
+            resultado += raiz.ToString();
+            if (raiz.hijoIzquierdo != null)
+            {
+                resultado += PreOrden(raiz.hijoIzquierdo);
+                if (raiz.hijoDerecho != null)
+                {
+                    resultado += PreOrden(raiz.hijoDerecho);
+                }
+            }
+            return resultado;
+        }
+    }
+}
---
+++ Nodo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbol_Binario
+{
+    class Nodo
+    {
+        private int _dato { get; }
+        public Nodo hijoIzquierdo;
+        public Nodo hijoDerecho;
+        public Nodo(int dato)
+        {
+            _dato = dato;
+        }
+        public int dato
+        {
+            get { return _dato; }
+        }
+        public override string ToString()
+        {
+            
+        }
+    }
+}
